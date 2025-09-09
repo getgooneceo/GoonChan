384673e6797544194236d08bc26b69a329f74ea5
SiddDevZ <epic.siddarth@gmail.com>
2025-09-10 02:24:58 +0530

    Added UMAMI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Poppins, Inter, Roboto } from "next/font/google";
-import { Toaster } from 'sonner';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Toaster } from "sonner";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 const inter = Inter({
-  variable: "--font-inter", 
+  variable: "--font-inter",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
@@ -32,7 +32,16 @@ export default function RootLayout({
       <head>
         <title>Goonchan</title>
         {/* <meta name="description" content="Adult content platform" /> */}
-        <script src='https://chaturbate.com/affiliates/promotools/popup/H5o9o/popchaturbaterevshare.js' type='text/javascript' defer></script>
+        <script
+          src="https://chaturbate.com/affiliates/promotools/popup/H5o9o/popchaturbaterevshare.js"
+          type="text/javascript"
+          defer
+        ></script>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="6220e997-c016-4b06-a191-476a13ca2184"
+        ></script>
       </head>
       <body
         className={`${poppins.variable} ${inter.variable} ${roboto.variable} antialiased`}
